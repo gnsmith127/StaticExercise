a2b717a0bb731d350aa86886f80242417486f169
Gianna Smith <gnsmith127@gmail.com>
2021-12-16 14:49:31 -0500

    finished exercise

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var celcius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelciusToFahrenheit(20);
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"Celcius: {celcius}");
+            Console.WriteLine($"Fahrenheit: {fahrenheit}");
+        }
+    }
+}
